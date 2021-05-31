858e07d48d00b058dc8e6799deb5c6f11652c009
domkesalles <felipe.salles@bandtec.com.br>
2021-05-31 00:58:36 -0300

    Linkando API de cadastro e login

---
+++ API-projeto-site/models/usuario.js
@@ -0,0 +1,50 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('Usuario',{
+		idUsuario: {
+			field: 'idUsuario',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nomeCompleto: {
+			field: 'nomeCompleto',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		dataNasc: {
+			field: 'dataNasc',
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+		fkJogadorFavorito: {
+			field: 'fkJogadorFavorito',
+			type: DataTypes.INTEGER,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'Usuario', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
